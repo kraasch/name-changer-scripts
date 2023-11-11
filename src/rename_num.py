d79b0fd1c68c7ff14f2ec56b9c31324a58a69970
Alex Kraasch <alex@kraasch.eu>
2023-11-11 13:04:38 +0100

    Make two runs to avoid renaming conflicts.

--- src/rename_num.py
+++ src/rename_num.py
@@ -8,15 +8,23 @@ if not len(sys.argv) == 2:
     print('provide a directory')
     exit()
 
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 dir=sys.argv[1]
 index=0
+prefix=id_generator()
 
-for file in sorted(pathlib.Path(dir).iterdir()):
-    index = index + 1
-    suffix = pathlib.Path(file).suffix
-    new_name=str(index).zfill(3)+str(suffix)
-    if not os.path.exists(new_name):
-        os.rename(file, new_name)
-    else:
-        print(f'Error: duplicate name at index {index}.')
-        break
+def rename_all(prefix):
+    for file in sorted(pathlib.Path(dir).iterdir()):
+        index = index + 1
+        suffix = pathlib.Path(file).suffix
+        new_name=dir+'/'+prefix+str(index).zfill(3)+str(suffix)
+        if not os.path.exists(new_name):
+            os.rename(file, new_name)
+        else:
+            print(f'Error: duplicate name at index {index}.')
+            break
+
+rename_all(prefix + '_')
+rename_all()
